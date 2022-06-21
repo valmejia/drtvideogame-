bad28f33e3e9efb7b4632d8250bd84635ee6cfe6
Val Mejia <valemegaestrellalectora@gmail.com>
2022-06-21 18:14:52 -0500

    igual de dificil que un cubo rubik para principiantes jajajaja

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('inicio')
 const ctx = canvas.getContext('2d')
-canvas.width = innerWidth
-canvas.height = innerHeight
+//canvas.width = innerWidth
+//canvas.height = innerHeight 
 
 
 /////////////////////////////////////// IMAGES ////////////////////////////////////////
@@ -29,6 +29,12 @@ pastita.src = "../img/pasta.png"
 const backimag = new Image()
 backimag.src = "../img/backmac.jpeg"
 
+ /////////////////////////////////// enemiessss ////////////////////////////////
+
+ const enemies = []
+
+
+
 ///////////////////////////// FUNCIONES PARA BOTONES /////////////////////////////////
 
 function quitarbuttons(){
@@ -41,10 +47,14 @@ function quitarbuttons(){
 }
 
 function StartGame(){
+    /////poner botones////
    quitarbuttons()
     canvas.classList.remove('noShow')
     console.log('empezar funciona')
+
+    ////poner fondo////
     updateCanvas()
+
 }
 
 function instrucciones(){
@@ -98,7 +108,7 @@ class personajes {
         this.velocidad = 1
         this.ctx = ctx
         this.img = img;
-        this.dibujarse()
+        this.dibujandoMonitos()
     }
 
     keepdamage(damage) {
@@ -133,25 +143,27 @@ class personajes {
         return false
     }
 
-    dibujarse() {
-        this.ctx.fillRect(this.x, this.y, 30, 30)
-        this.ctx.drawImage(this.img, this.x, this.y, 60, 60)
+    dibujandoMonitos() {
+        this.ctx.drawImage(this.img, this.x, this.y, 50, 50)
     }
 }
+
+
 ///////////////////////////// CLASES DE LOS PERSONAJES /////////////////////////////
 
-class drt extends personajes {
+class Drt extends personajes {
     constructor(x, y, ctx, image) {
         super(x, y, ctx, image)
         this.kills = 0
     }
 }
 
-class enemies extends personajes {
+class Enemies extends personajes{
     constructor(x, y, ctx, image) {
         super(x, y, ctx, image)
     }
 }
+
 //////////////////////////////////////// CLASE PASTA ////////////////////////////////
 class pastota {
     constructor(x, y, img, ctx) {
@@ -171,23 +183,22 @@ class pastota {
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case "ArrowLeft":
-            drtu.movetofront();  console.log('left'); 
+            drt.movetoback(); console.log('rigth');
             break;
         case "ArrowRight":
-            drtu.movetoback();
+            drt.movetofront();  console.log('left'); 
             break;
         case "ArrowDown":
-            drtu.bajar
+            drt.bajar(); console.log('bajando')
             break;
         case "Enter":
-            drtu.disparar();
+            drt.disparar(); console.log('disparo')
          break;
-        case "Space":
-           drtu.jump();
+        case " ":
+           drt.jump(); console.log('jumping')
             break;
     }
-  })
-
+})
 
 ////////////////////////////////// BACKGROUND IMAGES //////////////////////////////////
 
@@ -203,51 +214,93 @@ const backgroundImage = {
     },
 
     draw: function() {
-        ctx.drawImage(this.img, this.x, 0);
+        /*ctx.drawImage(this.img, this.x, 0,  innerWidth, innerHeight);
         if (this.speed < 0) {
-          ctx.drawImage(this.img, this.x + canvas.width, 0);
+          ctx.drawImage(this.img, this.x + canvas.width, 0, innerWidth, innerHeight);
         } else {
-          ctx.drawImage(this.img, this.x - this.img.width, 0);
+          ctx.drawImage(this.img, this.x - this.img.width, 0, innerWidth, innerHeight);
         }
-      },
+      },*/
+
+      ctx.drawImage(this.img, this.x, 0,);
+        if (this.speed < 0) {
+          ctx.drawImage(this.img, this.x + canvas.width, 0, );
+        } else {
+          ctx.drawImage(this.img, this.x - this.img.width, 0,);
+        }
+    },
 };
+// start calling updateCanvas once the image is loaded
+backimag.onload = updateCanvas;
+
+/////////////////////////////////// CAJAS DE LOS PERSONAJES ////////////////////////////
+const drt = new Drt(0,0, ctx, drtu) 
+
 
 function updateCanvas() {
     backgroundImage.move();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     backgroundImage.draw();
+    drt.dibujandoMonitos();
     requestAnimationFrame(updateCanvas);
-  }
+}
   
-  // start calling updateCanvas once the image is loaded
-  backimag.onload = updateCanvas;
-  
-///////////////////////////////////////// CANVAS 2/////////////////////////////////////
-  const backgroundImageCreditos = {
-    img: creditosimag, //background 
+//////////////////////////////// BACKGROUND IMAGES CREDITOS/////////////////////////////////////
+
+const backgroundImageCreditos = {
+    img: backimag, //background 
     x: 0,
     y: 0, 
+    speed: -1, 
     move: function(){
         this.y += this.speed;
-        this.y %= canvas.width;
         this.y %= canvas.height;
     },
 
     draw: function() {
         ctx.drawImage(this.img, this.y, 0);
         if (this.speed < 0) {
-          cty.drawImage(this.img, this.y + canvas.width, 0);
+          ctx.drawImage(this.img, this.y + canvas.height, 0);
         } else {
-          cty.drawImage(this.img, this.y - this.img.width, 0);
+          ctx.drawImage(this.img, this.y - this.img.height, 0);
         }
-      },
-  }
+    },
+};
 
-  function updateCanvascredits() {
-    backgroundImageCreditos.move();
-    cty.clearRect(0, 0, canvas.width, canvas.height);
-    backgroundImageCreditos.draw();
-    requestAnimationFrame(updateCanvascredits);
-  }
 
-  creditosimag.onload = updateCanvascredits;
+function updateCanvascredits() {
+  backgroundImageCreditos.move();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundImageCreditos.draw();
+  requestAnimationFrame(updateCanvascredits);
+};
+
+backimag.onload = updateCanvascredits;
+
+//////////////////////////////////////////////////////////////////////////
+
+//// FUNCTION VIDAS ////
+
+function liveshearts(vida){
+
+    ctx.drawImage(this.corazon)
+}
+
+
+///////////////////////////////////// crear enemigos //////////////////////////////////
+/*function makeEnemigo (){
+    const enemigoAleatorio = Math.floor(Math.random * canvas.height)
+    if(enemigoAleatorio){
+        console.log("Agrega un enemigo")
+        let typeOfEnemie = calixto
+    } elseif(enemigoAleatorio){
+        console.log("Agrega un enemigo")
+        let typeOfEnemie = carie
+    } else{
+        let typeOfEnemie = infeccion
+    }
+     const enemies = new Enemies(0,0, ctx, typeOfEnemie)
+     enemigos.push(enemies)
+}*/
+
+  
